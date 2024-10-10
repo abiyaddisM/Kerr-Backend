b69b22f322bd76ea02d231e169972a5ef2cb973b
Abiy Addis <abiymw17@gmail.com>
2024-10-10 22:10:21 +0300

    klsad

--- src/routes/uploadRouters.js
+++ src/routes/uploadRouters.js
@@ -5,8 +5,8 @@ const {postUpload,getUpload} = require( '../controllers/uploadController');
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination:function (req,file,cb){
-
-        cb(null,'upload')
+        // cb(null,'upload')
+        cb(null, path.join(__dirname, '..', 'upload'));
     },
     filename: function(req,file,cb){
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
