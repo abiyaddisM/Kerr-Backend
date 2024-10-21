5cb758397a066c4996c72e22b10601f3507f60e0
Abiy Addis <abiymw17@gmail.com>
2024-10-21 14:20:27 +0300

    klsad

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -39,9 +39,9 @@ exports.getAllJob = async (req,res) =>{
 exports.postJobCompletionRequest = async (req,res) =>{
     try{
         const {jobID} = req.params;
-        const {userID} = req.body;
-        const query = "CALL sp_InsertJobCompletionRequest(?,?)";
-        const [rows] = await pool.query(query,[userID,jobID]);
+        const {userID,image,message} = req.body;
+        const query = "CALL sp_InsertJobCompletionRequest(?,?,?,?)";
+        const [rows] = await pool.query(query,[userID,jobID,image,message]);
     }catch(e){
         console.log(' Error: ',e);
         res.status(500).send({message:"There was an issue with the server"});
