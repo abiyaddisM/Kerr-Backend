c1d80461f4299822a8ef3e726436ae3725aa83d3
Abiy Addis <abiymw17@gmail.com>
2024-10-23 16:14:49 +0300

    klsad

--- src/socketIO.js
+++ src/socketIO.js
@@ -1,7 +1,7 @@
 const socketIo = require('socket.io');
 
 let io;
-
+const onlineUsers = {};
 exports.initializeSocket  = (server)=>{
      io = socketIo(server, {
         cors: {
@@ -21,22 +21,28 @@ exports.initializeSocket  = (server)=>{
         })
 
         socket.on('online',({room,id})=>{
+            onlineUsers[socket.id] = id
             socket.join(room)
             io.to(room).emit('online',{online:true,userID:id});
             console.log(room, "Has joined and is Online")
         })
-        socket.on('offline',({room,id})=>{
-            io.to(room).emit('online',{online:false,userID:id});
-            socket.join(room)
-            console.log(room, "Someone has left")
-        })
+
         socket.on('joinOnlineRoom',({room})=>{
             socket.join(room)
             console.log(room, "Someone has joined To see you")
         })
 
         socket.on('disconnect', () => {
-            console.log('User disconnected');
+            const userID = onlineUsers[socket.id]; // Get user ID from the mapping
+            if (userID) {
+                // Optionally, you can find the room here if you need to notify specific rooms
+                const room = Object.keys(socket.rooms).find(r => r !== socket.id);
+                io.to(room).emit('online', { online: false, userID }); // Notify others that the user has gone offline
+                delete onlineUsers[socket.id]; // Remove the user from the online list
+                console.log('User disconnected:', userID);
+            } else {
+                console.log('User disconnected without a registered ID');
+            }
         });
     });
 
